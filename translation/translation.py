efb4b958db725797f44e8c7989642cda1fc35d61
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-04-22 11:44:03 +0900

    update: コード整形

--- translation/translation.py
+++ translation/translation.py
@@ -1,15 +1,18 @@
 from googletrans import Translator
 from const import LANGUAGES
 
+
 def get_code_by_name(language_name):
     for lang in LANGUAGES:
         if lang["name"] == language_name:
             return lang["code"]
     return None
 
+
 translator = Translator()
-language="Japanese"
-# 翻訳先の言語
-dest=get_code_by_name(language)
-translation = translator.translate('good evening. This is the code using googletrans.', dest=dest)
+src = 'good evening. This is the code using googletrans.'
+language_to_translate = "Japanese"
+code_to_translate = get_code_by_name(language_to_translate)
+translation = translator.translate(src, dest=code_to_translate)
+
 print(translation.text)
