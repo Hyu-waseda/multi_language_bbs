e9beb19932c4f3562f9ef679fbb72f539add975c
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-05-29 17:43:48 +0900

    update: thread getAPIのデータ整形部分を追加

--- backend/src/application/thread_application.py
+++ backend/src/application/thread_application.py
@@ -11,16 +11,42 @@ class ThreadApplication:
     def __init__(self, params: Params):
         self.params = params
 
-    # スレッドを返す関数
+    # データベースから取得した結果を指定の形式に整形する関数
+    def __format_thread_data(self, threads):
+        res = [
+            {
+                "ThreadID": thread_data[0],
+                "Title": thread_data[1],
+                "CreatedAt": thread_data[2],
+                "UpdatedAt": thread_data[3],
+                "UserID": thread_data[4],
+                "Content": thread_data[5],
+                "Language": thread_data[6],
+                "Views": thread_data[7],
+                "Likes": thread_data[8],
+                "Tags": thread_data[9],
+                "CategoryID": thread_data[10],
+                "ImageURL": thread_data[11]
+            }
+            for thread_data in threads
+        ]
+        return res
+
     def get_threads(self):
+        """
+        スレッドを取得する関数
+
+        :return: スレッドのリスト
+        """
         thread_infrastructure = ThreadInfrastructure()
 
         # スレッド数が指定されている場合、指定数のスレッドを返す
         if self.params["count"] is not None:
-            res = thread_infrastructure.fetch_threads_by_count(
+            threads = thread_infrastructure.fetch_threads_by_count(
                 self.params["count"])
+            res = self.__format_thread_data(threads=threads)
         # スレッド数が指定されていない場合、全てのスレッドを返す
         else:
-            res = thread_infrastructure.fetch_all_threads()
-
+            threads = thread_infrastructure.fetch_all_threads()
+            res = self.__format_thread_data(threads=threads)
         return res
