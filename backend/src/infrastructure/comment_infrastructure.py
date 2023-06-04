1b9e7212bc1199630c18967cd9c1f6b7cb62a892
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-06-04 15:15:19 +0900

    update: 不要なprint()の削除

--- backend/src/infrastructure/comment_infrastructure.py
+++ backend/src/infrastructure/comment_infrastructure.py
@@ -31,7 +31,6 @@ class CommentInfrastructure:
         params = (thread_id,)
         try:
             res = self.database_manager.execute_query(query, params)
-            print(res)
         except Exception as e:
             raise HTTPException(
                 status_code=500, detail="データベースからコメントを取得できませんでした。fetch_comments_by_thread_id: " + str(e))
