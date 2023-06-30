91e7db5ce8c9e4d5afc4818749b10e103a9836ba
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-06-30 19:53:41 +0900

    update: thread APIにソート機能(作成順、更新順)を追加

--- backend/src/infrastructure/thread_infrastructure.py
+++ backend/src/infrastructure/thread_infrastructure.py
@@ -7,29 +7,71 @@ class ThreadInfrastructure:
         self.database_manager = DatabaseManager()
 
     # 全てのスレッドを最新順に返す関数
-    def fetch_all_threads(self):
+    def fetch_all_threads_sorted_by_created_at(self):
+        """
+        全てのスレッドを作成日時の降順で取得する関数
+        :return: スレッドのリスト
+        """
         query = "SELECT * FROM Threads ORDER BY CreatedAt DESC;"
         try:
             res = self.database_manager.execute_query(query)
         except Exception as e:
             raise HTTPException(
-                status_code=500, detail="データベースからスレッドを取得できませんでした。fetch_all_threads: " + str(e))
+                status_code=500, detail="データベースからスレッドを取得できませんでした。fetch_all_threads_sorted_by_created_at: " + str(e))
+        return res
+    
+    def fetch_all_threads_sorted_by_updated_at(self):
+        """
+        全てのスレッドを更新日時の降順で取得する関数
+        :return: スレッドのリスト
+        """
+        query = "SELECT * FROM Threads ORDER BY UpdatedAt DESC;"
+        try:
+            res = self.database_manager.execute_query(query)
+        except Exception as e:
+            raise HTTPException(
+                status_code=500, detail="データベースからスレッドを取得できませんでした。fetch_all_threads_sorted_by_updated_at: " + str(e))
         return res
 
-    # 指定されたオフセットのスレッドをスレッド数、最新順に返す関数
-    def fetch_threads_by_offset(self, offset: int, count: int):
+    def fetch_threads_by_offset_sorted_by_created_at(self, offset: int, count: int):
+        """
+        指定されたオフセットと件数でスレッドを作成日時の降順で取得する関数
+        :param offset: オフセット値
+        :param count: 取得する件数
+        :return: スレッドのリスト
+        """
         query = "SELECT * FROM Threads ORDER BY CreatedAt DESC LIMIT %s OFFSET %s;"
         params = (count, offset)
         try:
             res = self.database_manager.execute_query(query, params)
         except Exception as e:
             raise HTTPException(
-                status_code=500, detail="データベースからスレッドを取得できませんでした。fetch_threads_by_offset: " + str(e))
+                status_code=500, detail="データベースからスレッドを取得できませんでした。fetch_threads_by_offset_sorted_by_created_at: " + str(e))
+        return res
+    
+    def fetch_threads_by_offset_sorted_by_updated_at(self, offset: int, count: int):
+        """
+        指定されたオフセットと件数でスレッドを更新日時の降順で取得する関数
+        :param offset: オフセット値
+        :param count: 取得する件数
+        :return: スレッドのリスト
+        """
+        query = "SELECT * FROM Threads ORDER BY UpdatedAt DESC LIMIT %s OFFSET %s;"
+        params = (count, offset)
+        try:
+            res = self.database_manager.execute_query(query, params)
+        except Exception as e:
+            raise HTTPException(
+                status_code=500, detail="データベースからスレッドを取得できませんでした。fetch_threads_by_offset_sorted_by_updated_at: " + str(e))
         return res
 
     # 特定のスレッドをIDで取得する関数
-
     def fetch_thread_by_id(self, thread_id: str):
+        """
+        指定されたスレッドIDに一致するスレッドを取得する関数
+        :param thread_id: スレッドのID
+        :return: スレッドの情報
+        """
         query = "SELECT * FROM Threads WHERE ThreadID = %s;"
         params = (thread_id,)
         try:
@@ -44,6 +86,10 @@ class ThreadInfrastructure:
 
     # スレッドの総数を取得する関数
     def fetch_thread_count(self):
+        """
+        スレッドの総数を取得する関数
+        :return: スレッドの総数
+        """
         query = "SELECT COUNT(*) FROM Threads;"
         try:
             res = self.database_manager.execute_query(query)
@@ -54,6 +100,11 @@ class ThreadInfrastructure:
         
     # スレッドの更新日時を更新する関数
     def update_thread_updated_at(self, thread_id: int, updated_at: str):
+        """
+        指定されたスレッドIDのスレッドの更新日時を更新する関数
+        :param thread_id: スレッドのID
+        :param updated_at: 更新日時
+        """
         query = """
             UPDATE Threads SET UpdatedAt = %s WHERE ThreadID = %s;
         """
--- backend/src/routers/thread_router.py
+++ backend/src/routers/thread_router.py
@@ -1,4 +1,6 @@
+from enum import Enum
 from typing import Optional
+from src.enums.sort_option import SortOption
 from src.application.thread_application import ThreadApplication
 from fastapi import APIRouter, Path, Query
 
@@ -6,13 +8,13 @@ router = APIRouter()
 
 
 @router.get("/thread")
-async def get_thread(offset: Optional[int] = Query(None, description="取得するスレッドのオフセット数"), count: Optional[int] = Query(None, description="取得するスレッド数")):
+async def get_thread(sort: SortOption = SortOption["new"], offset: Optional[int] = Query(0, description="取得するスレッドのオフセット数"), count: Optional[int] = Query(0, description="取得するスレッド数")):
     """
     スレッドを取得するAPI
 
     offset: 取得するスレッドのオフセット数（指定しない場合は最初から）
     """
-    params = {"offset": offset, "count": count}
+    params = {"offset": offset, "count": count, "sort": sort}
     thread_application = ThreadApplication(params=params)
     res = thread_application.get_threads()
     return res
