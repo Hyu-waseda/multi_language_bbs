6a9ddb3887dfc1e8a927d6128f68e836d0c61406
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-07-25 16:38:11 +0900

    update: Fotterの作成

--- frontend/components/organisms/Footer/Footer.tsx
+++ frontend/components/organisms/Footer/Footer.tsx
@@ -1,9 +1,33 @@
-import { Box } from "@mui/material";
+import React from "react";
+import { Box, Link, Typography } from "@mui/material";
+import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
   return (
-    <Box sx={{ backgroundColor: "#ADD8E6", padding: "1rem", textAlign: "center" }}>
-      <p>hoge</p>
+    <Box className={styles.footerContainer}>
+      <Link
+        href="/privacy-policy"
+        variant="body2"
+        className={styles.footerLink}
+      >
+        プライバシーポリシー
+      </Link>
+      <Link
+        href="/terms-of-service"
+        variant="body2"
+        className={styles.footerLink}
+      >
+        利用規約
+      </Link>
+      <Link href="/disclaimer" variant="body2" className={styles.footerLink}>
+        免責事項
+      </Link>
+      <Link href="/contact-us" variant="body2" className={styles.footerLink}>
+        お問い合わせ
+      </Link>
+      <Typography variant="body2" className={styles.footerCopyright}>
+        Copyright © 2023 早稲田大学 西村研究室
+      </Typography>
     </Box>
   );
 };
