7f2b2e531af9832c2e92aa30efc944ff623c4068
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-09-13 16:56:42 +0900

    update: スレッド投稿のフォームでCustomTextFieldを使用するように変更

--- frontend/pages/thread/create.tsx
+++ frontend/pages/thread/create.tsx
@@ -5,26 +5,20 @@ import { useForm } from "react-hook-form";
 import { useCookie } from "../../utils/useCookie";
 import { ThreadFormValues } from "../../interfaces/ThreadFormValues";
 import { sendThreadData } from "../../utils/api";
+import CustomTextField from "../../components/Atoms/CustomTextField/CustomTextField";
 
 const ThreadCreate: NextPage = () => {
   // Cookieから現在の言語設定を取得
   // TODO: 丸め込み
   const langCookie = useCookie("selectedLanguage");
 
-  // React Hook Formの設定
+  // スレッド投稿フォーム用
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm<ThreadFormValues>();
-
-  // Enterキーでの送信を防止するための関数
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-    }
-  };
 
   // フォーム送信時の処理
   const onSubmit = async (data: ThreadFormValues) => {
@@ -38,24 +32,6 @@ const ThreadCreate: NextPage = () => {
     reset();
   };
 
-  // テキストフィールドのレンダリング用関数
-  const renderTextField = (name: keyof ThreadFormValues, label: string) => (
-    <TextField
-      fullWidth
-      label={label}
-      {...register(name, {
-        required: `${label}は必須です`,
-        maxLength: {
-          value: 255,
-          message: `${label}は255文字以下で入力してください`,
-        },
-      })}
-      error={Boolean(errors[name])}
-      helperText={errors[name]?.message}
-      onKeyDown={handleKeyPress}
-    />
-  );
-
   return (
     <>
       <Header lang={langCookie} />
@@ -64,22 +40,28 @@ const ThreadCreate: NextPage = () => {
           新規スレッド作成
         </Typography>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Box mt={3}>{renderTextField("title", "タイトル")}</Box>
-          <Box mt={1}>{renderTextField("author", "作成者名")}</Box>
+          <Box mt={3}>
+            <CustomTextField
+              name="title"
+              label="タイトル"
+              register={register}
+              errors={errors}
+            />
+          </Box>
+          <Box mt={1}>
+            <CustomTextField
+              name="author"
+              label="作成者名"
+              register={register}
+              errors={errors}
+            />
+          </Box>
           <Box mt={1.5}>
-            <TextField
-              fullWidth
+            <CustomTextField
+              name="description"
               label="概要"
-              {...register("description", {
-                required: "概要は必須です",
-                maxLength: {
-                  value: 255,
-                  message: "概要は255文字以下で入力してください",
-                },
-              })}
-              error={Boolean(errors.description)}
-              helperText={errors.description?.message}
-              multiline
+              register={register}
+              errors={errors}
               rows={4}
             />
           </Box>
