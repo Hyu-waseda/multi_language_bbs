d4726fe638286357b4be13fb22605b16c3918e4a
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-11-17 20:07:58 +0900

    update: Meta.tsxにTwitterカードを追加

--- frontend/translate/es/pages/Index_es.tsx
+++ frontend/translate/es/pages/Index_es.tsx
@@ -1,4 +1,9 @@
 export const Index_ES = {
+  meta: {
+    title: "El canal",
+    description:
+      "El canal es un tablón de anuncios de traducción automática multilingüe.Puede comunicarse fácilmente con personas de todo el mundo.",
+  },
   latest_update_thread: "El último hilo de actualización",
   new_thread: "Nuevo hilo de llegada",
   update_date: "Día de actualización",
--- frontend/translate/fr/pages/Index_fr.tsx
+++ frontend/translate/fr/pages/Index_fr.tsx
@@ -1,4 +1,9 @@
 export const Index_FR = {
+  meta: {
+    title: "La chaîne",
+    description:
+      "Le canal est un babillard de traduction automatique multilingue.Vous pouvez facilement communiquer avec des gens du monde entier.",
+  },
   latest_update_thread: "Le dernier fil de mise à jour",
   new_thread: "Nouveau fil d'arrivée",
   update_date: "Jour de mise à jour",
