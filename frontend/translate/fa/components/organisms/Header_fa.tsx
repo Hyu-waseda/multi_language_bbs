1464d9adc3535ff7a96d78e0561accf49823451d
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-12-12 20:40:36 +0900

    update: fa(ペルシャ語)に対応

---
+++ frontend/translate/fa/components/organisms/Header_fa.tsx
@@ -0,0 +1,7 @@
+export const Header_FA = {
+  language: "زبان",
+  homepage: "صفحه نخست",
+  new_thread_creation: "ایجاد موضوع جدید",
+};
+
+export default Header_FA;
---
+++ frontend/translate/fa/pages/thread/Index_fa.tsx
@@ -0,0 +1,8 @@
+export const Index_FA = {
+  author: "نام سازنده",
+  comment: "اظهار نظر",
+  no_comment: "هنوز هیچ نظری ارسال نشده است",
+  submit: "ارسال",
+};
+
+export default Index_FA;
